278ced0ad8f99f7f8270e0e5a98061ba732d094d
KOOKDONGHUN <dh3978@naver.com>
2020-05-28 16:12:51 +0900

    20200528_1305 commit

--- keras/keras61_cifar10_dnn.py
+++ keras/keras61_cifar10_dnn.py
@@ -51,10 +51,10 @@ model.summary()
 # 3. 컴파일(훈련준비),실행(훈련)
 model.compile(optimizer='adam',loss = 'categorical_crossentropy', metrics = ['acc'])
 
-hist = model.fit(x_train,y_train,epochs=20,batch_size=300,callbacks=[],verbose=2,validation_split=0.2)
+hist = model.fit(x_train,y_train,epochs=20,batch_size=200,callbacks=[],verbose=2,validation_split=0.2)
 
 # 4. 평가, 예측
-loss,acc = model.evaluate(x_test,y_test,batch_size=300)
+loss,acc = model.evaluate(x_test,y_test,batch_size=200)
 
 print(f"loss : {loss}")
 print(f"acc : {acc}")
