fa604f8d967a14f4e11e2eb72b3970d5ee0be380
KOOKDONGHUN <dh3978@naver.com>
2020-05-30 14:38:10 +0900

    20200530_1438 commit

--- keras/keras73_boston_dnn.py
+++ keras/keras73_boston_dnn.py
@@ -25,9 +25,9 @@ print(f"y_data.shape : {y_data.shape}") # (506,)
 feature_names = a_data.feature_names
 print(f"feature_names : {feature_names}") # 13개의 칼럼
 
-# std = StandardScaler()
-# std.fit(x_data) # (13,3)
-# x_data = std.transform(x_data)
+std = StandardScaler()
+std.fit(x_data) # (13,3)
+x_data = std.transform(x_data)
 
 # pca = PCA(n_components=9)
 # pca.fit(x_data)
@@ -50,8 +50,17 @@ print(f"x_train.shape : {x_train.shape}") # x_train.shape : (404, 13)
 # 2. 모델
 model = Sequential()
 model.add(Dense(64,input_shape=(13,)))
+model.add(Dropout(0.5))
 model.add(Dense(64,activation='relu'))
-model.add(Dense(64,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(256,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(256,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(64,activation='relu'))
 model.add(Dense(1))
 
@@ -60,12 +69,11 @@ model.summary()
 # 3. 컴파일(훈련준비),실행(훈련)
 model.compile(optimizer='adam',loss = 'mse', metrics = ['mse'])
 
-hist = model.fit(x_train,y_train,epochs=75,batch_size=6,callbacks=[],verbose=2,validation_split=0.1)
+hist = model.fit(x_train,y_train,epochs=25,batch_size=3,callbacks=[],verbose=2,validation_split=0.03)
 
 
 
 # 4. 평가, 예측
-loss,acc = model.evaluate(x_test,y_test,batch_size=6)
 
 # R2 구하기 # 1에 근접할수록 좋다. 다른 보조지표와 같이 쓴다.
 from sklearn.metrics import r2_score
@@ -75,9 +83,9 @@ y_predict = model.predict(x_test)
 r2_y_predict = r2_score(y_test,y_predict)
 
 loss = hist.history['loss']
-acc = hist.history['mse']
+mse = hist.history['mse']
 val_loss = hist.history['val_loss']
-val_acc = hist.history['val_mse']
+val_mse = hist.history['val_mse']
 
 plt.figure(figsize=(10,6)) # -> 도화지의 크기? 출력되는 창의 크기인가 그래프의 크기인가 
 
@@ -107,7 +115,8 @@ plt.xlabel('epoch')
 plt.legend(['train mse','val mse'])
 
 plt.show()
+loss,mse = model.evaluate(x_test,y_test,batch_size=3)
 
+print(f"mse : {mse}") 
+print(f"loss : {loss}")
 print("r2 : ",r2_y_predict)
-# print(f"loss : {loss}") 
-# print(f"acc : {acc}") # acc : 
