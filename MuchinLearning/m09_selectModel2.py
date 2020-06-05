307e0007c807aa93ac0bd3e4aeb88c3bb7e465d2
KOOKDONGHUN <dh3978@naver.com>
2020-06-05 13:44:07 +0900

    20200605_1344 commit

--- MuchinLearning/m09_selectModel2.py
+++ MuchinLearning/m09_selectModel2.py
@@ -4,27 +4,39 @@ from sklearn.metrics import accuracy_score
 from sklearn.utils.testing import all_estimators
 import sklearn
 import warnings
+from sklearn.preprocessing import StandardScaler
 
 warnings.filterwarnings('ignore')
 
-iris = pd.read_csv('./data/csv/boston_house_prices.csv',sep=',',header=1)
+boston = pd.read_csv('./data/csv/boston_house_prices.csv',sep=',',header=1)
 
-x = iris.iloc[:, 0:13]
-y = iris.iloc[:, 13]
+print(boston)
+
+x = boston.iloc[:, :13].values
+y = boston.iloc[:, 13].values
 
 print(x)
 print(y)
 
+# y = y.reshape(-1)
+# print(y)
+
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size = 0.2,random_state=33)
 
-allAlgorithms = all_estimators(type_filter='regressor') # iris에 대한 모든 모델링
+std = StandardScaler()
+x_train = std.fit_transform(x_train)
+x_test = std.transform(x_test)
+
+allAlgorithms = all_estimators(type_filter='regressor') # 
 
 for (name, algorithm) in allAlgorithms:
-    model = algorithm() # -> 존나 희안한 문법인거 같은데 
+    model = algorithm() 
     print(sklearn.__version__)
-    model.fit(x_train,y_train)
-    y_pred = model.predict(x_test)
-    # print(name,"의 정답률 = ", accuracy_score(y_test,y_pred))
-    print(name,"의 정답률 = ", model.score(y_test,y_pred))
-
+    try : 
+        model.fit(x_train,y_train)
+        y_pred = model.predict(x_test)
+        # print(name,"의 정답률 = ", accuracy_score(y_test,y_pred))
+        print(name,"의 정답률 = ", model.score(x_test,y_test))
+    except:
+        print("Error!!",name)
 print(sklearn.__version__)
