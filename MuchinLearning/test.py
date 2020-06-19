9c21b7897bbd539f1183d17ed61ac1c98c6f88a5
KOOKDONGHUN <dh3978@naver.com>
2020-06-19 15:36:37 +0900

    20200619_1536 commit

---
+++ MuchinLearning/test.py
@@ -0,0 +1,3 @@
+import numpy as np
+a= list(np.arange(0.03,1,0.01))
+print(a)
---
+++ Z9test.py
@@ -0,0 +1,8 @@
+
+print(range(180, 250, 10))
+print(range(180, 250))
+
+ls = list(range(180.1, 181.1, 0.1 ))
+
+ls = [0.01]
+print(ls)
