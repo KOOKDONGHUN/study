51003f8571e304a62acea057260b2522dd68e210
KOOKDONGHUN <dh3978@naver.com>
2020-06-24 14:08:47 +0900

    20200624_1408commit

--- ttttttttttttt.py
+++ ttttttttttttt.py
@@ -1,77 +1,10 @@
-# 과적합 방지
-# 1. 훈련 데이터양을 늘린다.
-# 2. 피처수를 줄인다.
-# 3. regularization
+a = [0, 51, 52, 50, 49, 53, 16, 54, 70, 14, 17, 68, 15, 48, 55, 3, 4, 39, 45, 40, 35, 2, 37, 43]
+b = [70, 0, 35, 1, 34, 49, 48, 50, 51, 69, 52, 41, 13, 40, 33, 2, 68, 46, 44, 37, 45, 36, 39, 10]
+c = [69, 68, 67, 0, 34, 1, 64, 70, 31, 33, 35, 66, 2, 30, 13, 14, 15, 40, 3, 36, 16, 9, 17, 38, 4]
+d = [62, 63, 1, 56, 60, 57, 58, 55, 44, 65, 27, 50, 29, 45, 61, 69, 21, 37, 48, 2, 0, 70, 3, 19, 64, 36]
 
-from xgboost import XGBClassifier, plot_importance, XGBRegressor
-
-from sklearn.multioutput import MultiOutputRegressor
-
-from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-
-from sklearn.metrics import r2_score, mean_absolute_error
-
-from sklearn.preprocessing import RobustScaler
-import pandas as pd
-import numpy as np
-
-from MuchinLearning.m35_outliers3_KJICode import outliers
-
-# 데이터
-train = pd.read_csv('./data/dacon/comp1/train.csv')
-test = pd.read_csv('./data/dacon/comp1/test.csv')
-submission = pd.read_csv('./data/dacon/comp1/sample_submission.csv')
-
-
-
-
-x = train.loc[:, 'rho':'990_dst']
-train = outliers(x)
-
-# test = test.loc[:, 'rho':'990_dst']
-
-# y = train.loc[:, 'hhb':'na']
-
-# x = x.dropna(axis=1)
-# print(x.head(2))
-
-# test = test.dropna(axis=1)
-# print(test.head(2))
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-for i in range(len(x.columns)):
-    print(f'x : {x.columns[i]} \t test : {test.columns[i]}')
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.9,random_state=5)
-
-model = XGBRegressor()
-
-name_ls = ['hhb','hbo2','ca','na']
-tmp_dic = dict()
-
-for i in range(4):
-    model.fit(x_train,y_train.iloc[:, i])
-    test_pred = model.predict(x_test)
-    r2_test = r2_score(y_test.iloc[:, i], test_pred)
-    mae_test = mean_absolute_error(y_test.iloc[:, i], test_pred)
-
-    print(r2_test, "\t",mae_test)
-
-    true_pred = model.predict(test)
-    tmp_dic[name_ls[i]] = true_pred
-
-# # submit
-# df = pd.DataFrame(tmp_dic,range(10000,20000),columns=['hhb','hbo2','ca','na'])
-# df.to_csv('./submission_test.csv',index_label='id') """
+s = set([0, 51, 52, 50, 49, 53, 16, 54, 70, 14, 17, 68, 15, 48, 55, 3, 4, 39, 45, 40, 35, 2, 37, 43,
+        70, 0, 35, 1, 34, 49, 48, 50, 51, 69, 52, 41, 13, 40, 33, 2, 68, 46, 44, 37, 45, 36, 39, 10,
+        69, 68, 67, 0, 34, 1, 64, 70, 31, 33, 35, 66, 2, 30, 13, 14, 15, 40, 3, 36, 16, 9, 17, 38, 4,
+        62, 63, 1, 56, 60, 57, 58, 55, 44, 65, 27, 50, 29, 45, 61, 69, 21, 37, 48, 2, 0, 70, 3, 19, 64, 36])
+print(s)
