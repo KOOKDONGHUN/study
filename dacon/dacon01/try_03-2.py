79e4d7ff60107a08bf8d406bc5fdf2c0c4c80d12
KOOKDONGHUN <dh3978@naver.com>
2020-06-26 13:44:11 +0900

    20200626_1344 commit

--- dacon/dacon01/try_03-2.py
+++ dacon/dacon01/try_03-2.py
@@ -9,6 +9,8 @@ from sklearn.metrics import r2_score, mean_absolute_error
 import pandas as pd
 import numpy as np
 
+from sklearn.feature_selection import SelectFromModel
+
 train = np.load('./data/dacon/comp1/train.npy')
 test = np.load('./data/dacon/comp1/pred.npy')
 
@@ -50,11 +52,35 @@ kfold = KFold(n_splits=4, shuffle=True, random_state=66)
 model = XGBRegressor(n_jobs=6)
 model2 = XGBRegressor(n_jobs=6)
 
+name_ls = ['hhb','hbo2','ca','na']
+tmp_dic = dict()
+
+###
+model.fit(x_train,y_train[:, 2])
+thresholds_2 = np.sort(model.feature_importances_)
+
+model.fit(x_train,y_train[:, 3])
+thresholds_3 = np.sort(model.feature_importances_)
+
+###
+selection_2=SelectFromModel(model,threshold=thresholds_2[100],prefit=True)
+selection_3=SelectFromModel(model,threshold=thresholds_3[100],prefit=True)
+
+selection_x_train_2 = selection_2.transform(x_train)
+selection_x_train_3 = selection_3.transform(x_train)
+
+###
+selection_x_test_2 = selection_2.transform(x_test)
+selection_x_test_3 = selection_3.transform(x_test)
+
+###
+test_2 = selection_2.transform(test)
+test_3 = selection_3.transform(test)
+
+###
 model = GridSearchCV(model, parameters, cv = kfold)
 model2 = GridSearchCV(model2, parameters2, cv = kfold)
 
-name_ls = ['hhb','hbo2','ca','na']
-tmp_dic = dict()
 
 ## hbb, hbo2
 for i in range(2):
@@ -69,18 +95,30 @@ for i in range(2):
     y_pred = model.predict(test)
     tmp_dic[name_ls[i]] = y_pred
 
-## ca, na
-for i in range(2,4):
-    model.fit(x_train,y_train[:, i])
+## ca
+model2.fit(selection_x_train_2,y_train[:, 2])
 
-    y_test_pred = model.predict(x_test)
-    r2 = r2_score(y_test[:, i],y_test_pred)
-    print(f"r2 : {r2}")
-    mae = mean_absolute_error(y_test[:, i],y_test_pred)
-    print(f"mae : {mae}")
-    
-    y_pred = model.predict(test)
-    tmp_dic[name_ls[i]] = y_pred
+y_test_pred = model2.predict(selection_x_test_2)
+r2 = r2_score(y_test[:, 2],y_test_pred)
+print(f"r2 : {r2}")
+mae = mean_absolute_error(y_test[:, 2],y_test_pred)
+print(f"mae : {mae}")
+
+y_pred = model2.predict(test_2)
+tmp_dic[name_ls[i]] = y_pred
+
+## na
+model2.fit(selection_x_train_3,y_train[:, 3])
+
+y_test_pred = model2.predict(selection_x_test_3)
+r2 = r2_score(y_test[:, 3],y_test_pred)
+print(f"r2 : {r2}")
+mae = mean_absolute_error(y_test[:, 3],y_test_pred)
+print(f"mae : {mae}")
+
+y_pred = model2.predict(test_3)
+tmp_dic[name_ls[i]] = y_pred
+
 
 df = pd.DataFrame(tmp_dic,range(10000,20000),columns=['hhb','hbo2','ca','na'])
 
