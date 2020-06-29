f8e30cfb5ccce355576cdfeb193da5375190418c
KOOKDONGHUN <dh3978@naver.com>
2020-06-29 09:35:33 +0900

    1

--- Ztest/Z9test.py
+++ Ztest/Z9test.py
@@ -12,8 +12,8 @@ y = np.array([1,2,3,4,5,6,7,8,9,10])
 skf = KFold(n_splits=4, shuffle=True)
 
 for train, validation in skf.split(x,y):
-    print(train, validation)
     print()
+    print(train, validation)
     print(x[train])
     print(x[validation])
     # print()
