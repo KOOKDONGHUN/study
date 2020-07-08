b280de9a5f37513dbc8552420abcad3032443ccb
KOOKDONGHUN <dh3978@naver.com>
2020-07-08 14:15:51 +0900

    20200708_1415 commit

--- tf/tf08_mv1.py
+++ tf/tf08_mv1.py
@@ -1,27 +1,39 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-x = [1.,2.,3.]
-y = [3.,5.,7.]
+tf.set_random_seed(77)
 
-w = tf.placeholder(tf.float32)
+# 칼럼별로 가중치가 있어야한다
+x1_data = [73., 93., 89., 96., 73.]
+x2_data = [80., 88., 91., 98., 66.]
+x3_data = [75., 93., 90., 100., 70.]
 
-hypothesis = x * w
+y_data = [152., 185., 180., 196., 142.]
 
-cost = tf.reduce_mean(tf.square(hypothesis - y))
+x1 = tf.placeholder(tf.float32)
+x2 = tf.placeholder(tf.float32)
+x3 = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32)
 
-w_hist = []
-cost_hist = []
+w1 = tf.Variable(tf.random_normal([1]), name = 'weight1')
+w2 = tf.Variable(tf.random_normal([1]), name = 'weight2')
+w3 = tf.Variable(tf.random_normal([1]), name = 'weight3')
+b = tf.Variable(tf.random_normal([1]), name = 'bias')
 
-with tf.compat.v1.Session() as sess:
-    # sess.run(tf.compat.v1.global_variables_initializer())
+h = x1 * w1 + x2 * w2 + x3 * w3 + b
 
-    for i in range(-30,50):
-        curr_w = i * 0.1 # 그림의 간격
-        curr_cost = sess.run(cost, feed_dict={w:curr_w})
+cost = tf.reduce_mean(tf.square(h - y))
 
-        w_hist.append(curr_w)
-        cost_hist.append(curr_cost)
+opt = tf.train.GradientDescentOptimizer(learning_rate=0.00004)
+train = opt.minimize(cost)
 
-plt.plot(w_hist,cost_hist)
-plt.show()
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for step in range(2001):
+    cost_val, h_val, _ = sess.run([cost, h, train], feed_dict={x1 : x1_data, x2 : x2_data, x3 : x3_data, y : y_data})
+
+    if step % 20 == 0:
+        print(step, 'cost : ', cost_val, '\n', h_val)
+
+sess.close()
