4d6f5a2d46db061e7ea529cc699e3f71f2b1012b
KOOKDONGHUN <dh3978@naver.com>
2020-07-13 14:09:56 +0900

    20200713_1409 commit

--- Ztest/r1.py
+++ Ztest/r1.py
@@ -57,7 +57,7 @@ print(_data)
 print(_data.shape)
 
 x_data = _data[:, :-1]
-y_data = _data[:, -1]
+y_data = _data[:, 1:]
 
 print(x_data)
 print(x_data.shape)
@@ -66,7 +66,6 @@ print(y_data)
 print(y_data.shape)
 
 x_data = x_data.reshape(x_data.shape[0],x_data.shape[1],1)
-y_data = y_data.reshape(-1,1)
 
 print(x_data.shape)
 print(y_data.shape)
@@ -78,11 +77,11 @@ print(sequence_length)
 print(input_dim)
 
 X = tf.compat.v1.placeholder(tf.float32, (None, sequence_length, input_dim))
-Y = tf.compat.v1.placeholder(tf.int32, (None, input_dim))
+Y = tf.compat.v1.placeholder(tf.int32, (None, sequence_length))
 
 print(X)
 print(Y)
-
+''' 
 # model
 output_node = 1
 batch_size = x_data.shape[0] # 전체 행??
@@ -90,19 +89,18 @@ lr = 0.01
 
 cell = tf.keras.layers.LSTMCell(output_node) # _lstm  ## 여기가 첫번째 연산
 h, _states = tf.nn.dynamic_rnn(cell, X, dtype=tf.float32) # == model.add(LSTM()) ## 여기가 두번째 연산?
-print(h) # Tensor("rnn/transpose_1:0", shape=(?, 3, 1), dtype=float32)
+print(h) # Tensor("rnn/transpose_1:0", shape=(?, 6, 5), dtype=float32)
+
 
 w = tf.ones([batch_size, sequence_length]) # == Y.shape // 1로 해도 값의 변화가 크지 않다 
-
 sequence_loss = tf.contrib.seq2seq.sequence_loss(logits=h, targets=Y, weights=w) #LSTM에서는 loss를 무조건 이거를 써야한다?  mse를 풀어썻다? // loss = h - y 가 기본
-
 cost = tf.reduce_mean(sequence_loss)
 
 train = tf.compat.v1.train.AdamOptimizer(learning_rate=lr).minimize(cost)
 
 prediction = tf.math.argmax(h, axis=2)
 
-'''
+
 with tf.compat.v1.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for i in range(201):
