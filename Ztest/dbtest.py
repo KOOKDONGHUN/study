95c1d47744361e2feb9da2a89ac5b8277761a532
KOOKDONGHUN <dh3978@naver.com>
2020-07-15 19:02:38 +0900

    20200715_1902 commit

---
+++ Ztest/dbtest.py
@@ -0,0 +1,16 @@
+import pymssql as ms
+print(ms.__version__)
+
+conn = ms.connect(server='192.168.0.176', user='bit2', password='1234',database='bitdb')
+
+cursor = conn.cursor()
+
+cursor.execute('SELECT * from credit_card_data;')
+
+row = cursor.fetchone()
+
+while row :
+    print(f'첫 컬럼 : {row[0]}\t 둘컬럼 : {row[1]}')
+    row = cursor.fetchone()
+
+conn.close()
