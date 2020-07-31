21e51cd517119bc775631852f04d2ae4037527aa
KOOKDONGHUN <dh3978@naver.com>
2020-07-31 11:59:42 +0900

    20200731_1159 commit

--- ANSWERBOT_Project/wikidocs_NLP/naver_crawlling2.py
+++ ANSWERBOT_Project/wikidocs_NLP/naver_crawlling2.py
@@ -53,10 +53,11 @@ data_col_name = ['id','que_title', 'que_detail', 'ans_writer','ans_detail']
 
 # 전체 페이지의 개수와 페이지당 개수 입력
 num_per_page = range(1,21)
-pages = range(137,200)
+pages = range(259,300)
 
-# num_per_page = range(2,21)
-# pages = [136]
+# num_per_page = range(1,21)
+# num_per_page = range(15,21)
+# pages = [258]
 
 # db없으면 생성
 create_table(tablename)
