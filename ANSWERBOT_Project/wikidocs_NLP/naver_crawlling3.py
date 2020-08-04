9347585a4f704bfe2e9deba7396a0d4cefa7c560
KOOKDONGHUN <dh3978@naver.com>
2020-08-04 11:15:59 +0900

    20200804_1115 commit

--- ANSWERBOT_Project/wikidocs_NLP/naver_crawlling3.py
+++ ANSWERBOT_Project/wikidocs_NLP/naver_crawlling3.py
@@ -50,7 +50,7 @@ data_col_name = ['id','que_title', 'que_detail', 'ans_writer','ans_detail']
 
 # 전체 페이지의 개수와 페이지당 개수 입력
 num_per_page = range(1,21)
-pages = range(301,341)
+pages = range(341,381)
 
 # num_per_page = range(1,21)
 # num_per_page = range(4,21)1
