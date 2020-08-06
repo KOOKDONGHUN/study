fd2ef92bf68f2a3ce7bc866dd4e7074fda3f4fcf
KOOKDONGHUN <dh3978@naver.com>
2020-08-06 23:19:40 +0900

    20200806_2319 commit

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import urllib.parse
 
-encode = '%2BiGFfCq9V7ttCQMaerGI3Uki53svVwaY1w%2BB6%2BDsu6A%3D'
+# encode = '%2BiGFfCq9V7ttCQMaerGI3Uki53svVwaY1w%2BB6%2BDsu6A%3D'
+        # +iGFfCq9V7ttCQMaerGI3Uki53svVwaY1w+B6+Dsu6A=
+
+encode = 'https://kin.naver.com/userinfo/answerList.nhn?u=%2BiGFfCq9V7ttCQMaerGI3Uki53svVwaY1w%2BB6%2BDsu6A%3D&isSearch=true&query=%EC%9E%90%EA%B2%A9%EC%A6%9D&sd=answer&y=0&section=qna&isWorry=false&x=0&page=1'
 decode = urllib.parse.unquote(encode)
 print(decode)
