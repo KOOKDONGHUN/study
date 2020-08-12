c8e56187203a6011766aa502e43f3740457409ab
KOOKDONGHUN <dh3978@naver.com>
2020-08-12 19:31:09 +0900

    20200812_1931 commit

--- ANSWERBOT_Project/naver_crawlling3.py
+++ ANSWERBOT_Project/naver_crawlling3.py
@@ -50,7 +50,7 @@ data_col_name = ['id','que_title', 'que_detail', 'ans_writer','ans_detail']
 
 # 전체 페이지의 개수와 페이지당 개수 입력
 num_per_page = range(1,21)
-pages = range(1425,1451)
+pages = range(1481,1511)
 
 # num_per_page = range(1,21)
 # num_per_page = range(9,21)
