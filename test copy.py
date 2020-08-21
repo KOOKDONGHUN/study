eb3a15fb4158a3b963acd18973271806834f88fe
KOOKDONGHUN <dh3978@naver.com>
2020-08-21 11:16:34 +0900

    20200821_1116 commit

--- test copy.py
+++ test copy.py
@@ -1,7 +1,80 @@
+from tensorflow import keras
+from keras import models
+from keras import layers
+from keras import optimizers, losses, metrics
+from keras import preprocessing
+
+import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import pickle
+import os
+import re
 
-data = pd.read_csv("D:/Study/ANSWERBOT_Project/data/urls_Certificate.txt",sep=',')
-print(data)
+from konlpy.tag import Okt
 
-data = data.drop_duplicates()
-print(data)
+# 입력 문장의 인덱스 시퀀스를 입력으로 받음
+encoder_inputs = layers.Input(shape=(None,)) # (1425, 78)
+
+# 임베딩 레이어
+encoder_outputs = layers.Embedding(6406, 100)(encoder_inputs) # (1425, 6406, 100)
+
+# return_state가 True면 상태값 리턴
+# LSTM은 state_h(hidden state)와 state_c(cell state) 2개의 상태 존재
+encoder_outputs, state_h, state_c = layers.LSTM(128, # 128
+                                                dropout=0.1,
+                                                recurrent_dropout=0.5,
+                                                return_state=True)(encoder_outputs)
+
+# 히든 상태와 셀 상태를 하나로 묶음
+encoder_states = [state_h, state_c]
+
+
+
+#--------------------------------------------
+# 훈련 모델 디코더 정의
+#--------------------------------------------
+
+# 목표 문장의 인덱스 시퀀스를 입력으로 받음
+decoder_inputs = layers.Input(shape=(None,))
+
+# 임베딩 레이어
+decoder_embedding = layers.Embedding(6406, 100)
+decoder_outputs = decoder_embedding(decoder_inputs)
+
+# 인코더와 달리 return_sequences를 True로 설정하여 모든 타임 스텝 출력값 리턴
+# 모든 타임 스텝의 출력값들을 다음 레이어의 Dense()로 처리하기 위함
+decoder_lstm = layers.LSTM(128,
+                           dropout=0.1,
+                           recurrent_dropout=0.5,
+                           return_state=True,
+                           return_sequences=True)
+
+# initial_state를 인코더의 상태로 초기화
+decoder_outputs, _, _ = decoder_lstm(decoder_outputs,
+                                     initial_state=encoder_states)
+
+# 단어의 개수만큼 노드의 개수를 설정하여 원핫 형식으로 각 단어 인덱스를 출력
+decoder_dense = layers.Dense(6406, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+
+
+
+#--------------------------------------------
+# 훈련 모델 정의
+#--------------------------------------------
+
+# 입력과 출력으로 함수형 API 모델 생성
+model = models.Model([encoder_inputs, decoder_inputs], decoder_outputs)
+
+# 학습 방법 설정
+model.compile(optimizer='rmsprop',
+              loss='categorical_crossentropy',
+              metrics=['acc'])    
+
+model.summary()
+
+
+# Open the file
+with open('./report.txt','w') as fh:
+    model.summary(print_fn=lambda x: fh.write(x + '\n'))
