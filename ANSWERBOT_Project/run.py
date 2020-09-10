67091b32bdd4fbf070dd72092c8b031e54e42f4c
KOOKDONGHUN <dh3978@naver.com>
2020-09-11 00:06:38 +0900

    20200911_0006 commit

--- ANSWERBOT_Project/run.py
+++ ANSWERBOT_Project/run.py
@@ -1,4 +1,11 @@
+#--------------------------------------------------------------------------------
+# Name    : run.py
+# Creator  : Jongha Woo
+#--------------------------------------------------------------------------------
+
 from flask import Flask, request, render_template
+
+# -*- coding: utf-8 -*-
 import argparse
 import logging
 import time
@@ -14,15 +21,17 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.core.lightning import LightningModule
 from torch.utils.data import DataLoader, Dataset
 from transformers.optimization import AdamW, get_cosine_schedule_with_warmup
+import kss
 
 ques = None
 ans = None
 
 parser = argparse.ArgumentParser(description='Simsimi based on KoGPT-2')
 
-parser.add_argument('--chat',
+
+parser.add_argument('--chat',           # chat없으면 False, 즉 실행 안하겠다.
                     action='store_true',
-                    default=False,
+                    default=False, 
                     help='response generation on given user input')
 
 parser.add_argument('--sentiment',
@@ -30,57 +39,73 @@ parser.add_argument('--sentiment',
                     default='0',
                     help='sentiment for system. 0 is neutral, 1 is negative, 2 is positive.')
 
-parser.add_argument('--model_params',
+parser.add_argument('--model_params', # type은 str
                     type=str,
-                    default=r'C:\Users\Administrator\KoGPT2-chatbot\model_chp\model_last.ckpt',
+                    default=r'D:/KoGPT2-chatbot/model_chp\model_preprocessinglast.ckpt',
                     help='model binary for starting chat')
 
-parser.add_argument('--train',
+parser.add_argument('--train',          # train없으면 False, 즉 실행 안하겠다.
                     action='store_true',
                     default=False,
                     help='for training')
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger = logging.getLogger()        # 값이 제대로 나오고 있는 지 확인, print안쓰고 logging하면 화면으로도 되고 파일로도 찍을 수 있다. 
+logger.setLevel(logging.INFO)       # 보통 logging많이 쓴다.(화면으로만 보이는 거는 print), lightning_logs는 다른거임
 
-U_TKN = '<usr>'
-S_TKN = '<sys>'
-BOS = '<s>'
-EOS = '</s>'
-MASK = '<unused0>'
-SENT = '<unused1>'
+# 기본 토큰
+# 마스크랑 sent는 챗봇에서만 쓰지, kogpt에서는 쓰지 않음, masked self-attention은 태그 마스크가 아님, bert의 단어 마스크랑 헷갈리지 말 것
+# 챗봇에서만 쓰는 mask태그
+U_TKN = '<usr>'                     # 질문
+S_TKN = '<sys>'                     # 답변
+BOS = '<s>'                         # 시작
+EOS = '</s>'                        # 끝
+MASK = '<unused0>'                  # 마스크
+SENT = '<unused1>'                  # 감정
 
+# model 의 input을 만들어주는 class
+# pytorch에서 dataloader씀(batch_size, shuffling, augmentation-데이터 증식 이런 기능들이 있음(image generator))
+# Dataset 찾아보기
 
-class CharDataset(Dataset):
-    def __init__(self, chats, tok_path, vocab, max_len=32):
-        self._data = chats
-        self._tok_path = tok_path
-        self.tokenizer = None
-        self.first = True
-        self.q_token = U_TKN
-        self.a_token = S_TKN
-        self.sent_token = SENT
-        self.bos = BOS
+# class 내부에서만, 외부에서는 못쓴다, 함수로 호출해서 변수를 불러서 리턴값 주는 것
+# class getter, setter 찾아보기
+# __init__: 클래스 초기화(클래스 만들 떄 첫번째에 써주는 것)
+class CharDataset(Dataset): #상속받으려는 클라스 Dataset
+    def __init__(self, chats, tok_path, vocab, max_len=32): # 파라미터 받는 것, 이 값을 가지고 초기화
+        self._data = chats          # csv파일
+        self._tok_path = tok_path   # tok_path: 토크나이저 위치
+        self.tokenizer = None       # tokenizer에 tok_path들어감
+        self.first = True           # logging. info로 처음에 데이터 보여주는 거(첫번째 배치만 보여주려고)
+        self.q_token = U_TKN        # q_token은 <usr>
+        self.a_token = S_TKN        # a_token은 <sys>
+        self.sent_token = SENT      # sent_token은 <sent>
+        self.bos = BOS              # 여기서 BOS는 안 씀 대신 <usr>, <sys>씀/// 처음이<usr> 끝 <EOS>// 처음이 <sys> 끝<EOS>
         self.eos = EOS
-        self.maskt = MASK
-        self.vocab = vocab
-        self.max_len = max_len
+        self.maskt = MASK           # label만들 때 쓰는 것(더미 토큰들 다 mask로 지정함)
+        self.vocab = vocab          
+        self.max_len = max_len      # max_len은 sequence(최대 받을 수 있는 길이)
         self.padder = nlp.data.PadSequence(
-            max_len, pad_val=self.vocab[self.vocab.padding_token])
+            max_len, pad_val=self.vocab[self.vocab.padding_token]) # pad_val은 padding에 무엇을 넣겠다 지정하는 것
+        
+        # 클래스의 속한 함수라고알려주는 것(self.) : class안의 전역변수 임을 의미
 
     def _activate_sp(self):
-        self.tokenizer = nlp.data.SentencepieceTokenizer(self._tok_path, 0, 0)
+        self.tokenizer = nlp.data.SentencepieceTokenizer(self._tok_path, 0, 0) # 토크나이저는 형태소 분석기 아님, 캐릭터 기반과 형태소 기반의 중간형태
+                                                                                # 코퍼스 학습을 계속 해나감, 어떤 토큰을 합칠까 계속 계산
+                                                                                # 자주 나오는 토큰은 계속 붙어 있다
+                                                                                # 1. 인, 공, 지, 능===> 인공, 지능(빈도수에 따라서 붙는다)
+                                                                                # 2. 인공지능 (하나의 토큰으로 생성됨)
+                                                                                # 이 정보가 token_path에 들어가 있다.
 
-    def __len__(self):
-        return len(self._data)
+    def __len__(self):                  # __len__: override datasets라는 클라스 안에 존재하는 함수임(__len__)   
+        return len(self._data)          # csv 데이터 개수 받는 것
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx):         # __getitem__: datasets 클라스 안에 존재하는 함수// 가져올떄 우리가 customize해서 가져오는 것
         if self.tokenizer is None:
             self._activate_sp()
-        turn = self._data.iloc[idx]
-        q = turn['Q']
-        a = turn['A']
-        sentiment = str(turn['label'])
+        turn = self._data.iloc[idx]    # ex) 10번째 데이터 받겠다. 10번째 가져온 게 turn에 들어간다, pandas의 10번째 데이터, 호출될때마다 계산
+        q = turn['Q']                  # 질문
+        a = turn['A']                  # 답변
+        sentiment = str(turn['label']) # 라벨
         q_toked = [
             self.q_token,
         ] + self.tokenizer(q) + [
@@ -90,20 +115,25 @@ class CharDataset(Dataset):
         ]
         q_len = len(q_toked)
         a_toked = [
-            self.a_token,
+            self.a_token,  #<sys>
         ] + self.tokenizer(a) + [
             self.eos,
         ]
         a_len = len(a_toked)
         if q_len + a_len > self.max_len:
-            remains = self.max_len - q_len
-            a_len = remains
-            a_toked = a_toked[-a_len:]
-            assert a_len == len(a_toked)
+            a_len = self.max_len - q_len
+            if a_len <= 0:
+                q_toked = q_toked[-(int(self.max_len/2)):]
+                q_len = len(q_toked)
+                a_len = self.max_len - q_len
+                assert a_len > 0 # 한번 더 확실하게 보장
+            a_toked = a_toked[:a_len]
+            a_len = len(a_toked)
+            assert a_len == len(a_toked), f'{a_len} ==? {len(a_toked)}'
         # [mask, mask, ...., mask, ..., <bos>,..A.. <eos>, <pad>....]
         labels = [
-            self.maskt,
-        ] * q_len + a_toked[1:]
+            self.maskt,             # 라벨은 a_token, 앞에 question길이 만큼 더미변수를 마스크
+        ] * q_len + a_toked[1:]     # a_token[1:]은 sys토큰 빼주는 것
         if self.first:
             logging.info("contexts : {}".format(q))
             logging.info("toked ctx: {}".format(q_toked))
@@ -111,33 +141,33 @@ class CharDataset(Dataset):
             logging.info("toked response : {}".format(a_toked))
             logging.info('labels {}'.format(labels))
             self.first = False
-        mask = [0] * q_len + [1] * a_len + [0] * (self.max_len - q_len - a_len)
-        return (self.padder(self.vocab[q_toked + a_toked]), np.array(mask),
+        mask = [0] * q_len + [1] * a_len + [0] * (self.max_len - q_len - a_len) # 정답부분만 1
+        return (self.padder(self.vocab[q_toked + a_toked]), np.array(mask),   # gpt2의 input, mask(진짜 레이블의 위치값 더미값 빼고), 출력이 나오는 모든 값 
                 self.padder(self.vocab[labels]))
 
 
-class KoGPT2Chat(LightningModule):
-    def __init__(self, hparams, **kwargs):
+class KoGPT2Chat(LightningModule): # pytorch lightning
+    def __init__(self, hparams, **kwargs): 
         super(KoGPT2Chat, self).__init__()
-        self.hparams = hparams
+        self.hparams = hparams     # hparams에 args정보 들어감
         self.tok_path = get_tokenizer()
         self.neg = -1e18
-        self.kogpt2, self.vocab = get_pytorch_kogpt2_model()
-        self.loss_function = torch.nn.CrossEntropyLoss(reduction='none')
-
+        self.kogpt2, self.vocab = get_pytorch_kogpt2_model() # 모델이랑 단어 사전 두개로 받아준다
+        self.loss_function = torch.nn.CrossEntropyLoss(reduction='none') # 손실함수는 CrossEntropyLoss : 분류 모델(label(정답값)과 gpt2의 아웃풋(원핫인코딩))
+                                                                         # 원핫인코딩과 index비교...?(함수 내부에서 index를 one_hot_encoding으로 바꿔서 crossentropy)
     @staticmethod
     def add_model_specific_args(parent_parser):
         # add model specific args
         parser = argparse.ArgumentParser(parents=[parent_parser], add_help=False)
         parser.add_argument('--max-len',
                             type=int,
-                            default=400,
-                            help='max sentence length on input (default: 32)')
+                            default = 800,
+                            help='max sentence length on input (default: 400)')
 
         parser.add_argument('--batch-size',
                             type=int,
-                            default=1,
-                            help='batch size for training (default: 96)')
+                            default = 1,
+                            help='batch size for training (default: 4)')
         parser.add_argument('--lr',
                             type=float,
                             default=5e-5,
@@ -150,15 +180,15 @@ class KoGPT2Chat(LightningModule):
 
     def forward(self, inputs):
         # (batch, seq_len, hiddens)
-        output, _ = self.kogpt2(inputs)
-        return output
+        output, _ = self.kogpt2(inputs) # 첫번째 input_ids
+        return output                   # (batch, sequence, one_hot_encoding) 50,000개의 one_hot_encoding
 
-    def training_step(self, batch, batch_idx):
-        token_ids, mask, label = batch
-        out = self(token_ids)
+    def training_step(self, batch, batch_idx): # 파이토치 라이트닝에 있는 것
+        token_ids, mask, label = batch         # 배치에서 세개 뽑아내는 것 
+        out = self(token_ids)                  # forward실행, token_ids가 input, 결과는 batch, sequence, one_hot_encoding(self대신 forward써도 돌아감)
         mask_3d = mask.unsqueeze(dim=2).repeat_interleave(repeats=out.shape[2], dim=2)
         mask_out = torch.where(mask_3d == 1, out, self.neg * torch.ones_like(out))
-        loss = self.loss_function(mask_out.transpose(2, 1), label)
+        loss = self.loss_function(mask_out.transpose(2, 1), label) # 원핫인코딩, label(index)
         loss_avg = loss.sum() / mask.sum()
         tensorboard_logs = {'train_loss': loss_avg}
         return {'loss': loss_avg, 'log': tensorboard_logs}
@@ -191,8 +221,8 @@ class KoGPT2Chat(LightningModule):
         return torch.LongTensor(data), torch.LongTensor(mask), torch.LongTensor(label)
 
     def train_dataloader(self):
-        data = pd.read_csv('Chatbot_data/ChatbotData.csv')
-        self.train_set = CharDataset(data, self.tok_path, self.vocab, max_len=self.hparams.max_len)
+        data = pd.read_csv("./naver_worry_finally_0824_preprocess_yes_null.csv",encoding='cp949')
+        self.train_set = CharDataset(data, self.tok_path, self.vocab, max_len=self.hparams.max_len) # self.hparams는 argument
         train_dataloader = DataLoader(
             self.train_set, batch_size=self.hparams.batch_size, num_workers=0,
             shuffle=True, collate_fn=self._collate_fn)
@@ -202,46 +232,69 @@ class KoGPT2Chat(LightningModule):
         global ques
         global ans
         self.tok_path
+        print("입력문장:",ques)
         tok = SentencepieceTokenizer(self.tok_path, num_best=0, alpha=0)
         sent_tokens = tok(sent)
-        print('입력문장 : ', ques)
-        print('입력문장 : ', type(ques))
         with torch.no_grad():
-            st_t = time.time()
-            des_t = st_t
+            # q = input('user > ').strip()
+            q = ques
+            q_tok = tok(q)
+            a = ''
+            a_tok = []
+            timeout = time.time() + 60
+            print(1)
             while 1:
-                q = ques
-                if q == 'quit':
+                input_ids = torch.LongTensor([
+                    self.vocab[U_TKN]] + self.vocab[q_tok] +
+                    self.vocab[EOS, SENT] + self.vocab[sent_tokens] +
+                    self.vocab[EOS, S_TKN] +
+                    self.vocab[a_tok]).unsqueeze(dim=0)
+                pred = self(input_ids)
+                gen = self.vocab.to_tokens(
+                    torch.argmax(
+                        pred,
+                        dim=-1).squeeze().numpy().tolist())[-1]
+                if gen == EOS:
                     break
-                q_tok = tok(q)
-                a = ''
-                a_tok = []
-                while 1:
-                    des_t = des_t + 1
-                    input_ids = torch.LongTensor([
-                        self.vocab[U_TKN]] + self.vocab[q_tok] +
-                        self.vocab[EOS, SENT] + self.vocab[sent_tokens] +
-                        self.vocab[EOS, S_TKN] +
-                        self.vocab[a_tok]).unsqueeze(dim=0)
-                    pred = self(input_ids)
-                    total = self.vocab.to_tokens(
-                        torch.argmax(
-                            pred,
-                            dim=-1).squeeze().numpy().tolist())
-                    gen = total[-1]
-                    if gen == EOS or len(total)>=200:
-                        break
-                    elif (des_t - st_t) > 100 :
-                        break
-                    a += gen.replace('▁', ' ')
-                    print(a)
-                    a_tok = tok(a)
-                if (des_t - st_t) > 100 :
-                    ans = '답변할 수 없습니다.'
-                    break
-                ans = a
+                a += gen.replace('▁', ' ')
+                a_tok = tok(a)
+                if time.time() > timeout:
+                    ans = "답변할 수 없습니다."
+                    break   
+            print(2)
+            answer_list = kss.split_sentences(a)[1:-2]
+            Simsimi_answer = "".join(answer_list)
+            sentence_list = Simsimi_answer.split('.')
+            sentences=[]
+            for s in sentence_list:
+                word_list = s.split()#리스트
+                # sentences=[]
+                for word in word_list:
+                    if word.endswith('*님이')==True:
+                        word_list[word_list.index(word)]= word.replace(word,"상담자님이")
 
+                    if word.endswith("비공개님!")==True:
+                        word_list[word_list.index(word)] = word.replace(word,"상담자님")
 
+                    if word.endswith('비공개님이')==True:
+                        word_list[word_list.index(word)]= word.replace(word,"상담자님이")
+
+                sentence = " ".join(word_list)
+                sentences.append(sentence)
+            ls = ['청소년사이버상담센터', '채팅상담', '=', 'https://www.', "전화상담", 'cyber', 'kr']
+            for sentence in sentences:
+                for s in ls:
+                    if s in sentence:
+                        sentences.remove(sentence)
+
+                # print(sentence)        
+            # print("Simsimi > ", ".".join(sentences))
+            ans = ". ".join(sentences)
+            print("답변:", ans)
+            # for sent in sentences:
+            #     ans+=sent
+            # ans=a
+print(3)
 parser = KoGPT2Chat.add_model_specific_args(parser)
 parser = Trainer.add_argparse_args(parser)
 args = parser.parse_args()
@@ -258,25 +311,24 @@ app = Flask(__name__)
 def show_page():
     global ques
     global ans
-
     #--------------------------------
     # 파라미터 설정
     #--------------------------------
     text = request.args.get("text")
     print(text)
-    print(type(text))
    
     #--------------------------------
     # 대답 구함
     #--------------------------------
     if text != None:
         ques = text
+        ans = ans
         model = KoGPT2Chat.load_from_checkpoint(args.model_params)
         model.chat()
     else:
         answer = None
     
-    return render_template('home.html', question = text, answer = ans)
+    return render_template('test.html', question = ques, answer = ans)
 
 
 
@@ -285,4 +337,5 @@ def show_page():
 #----------------------------------------------------
 if __name__ == "__main__":
 
-    app.run(host='127.0.0.1', port = 8080,  threaded=True)
+    app.run(host='127.0.0.1', port = 5000, threaded=True,debug=True)    
+
