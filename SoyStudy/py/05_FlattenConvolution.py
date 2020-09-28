33be5a3f06ea0a04e659ae1f845aa4d25b2d7fd1
국동훈 <kookdonghun@gugdonghun-ui-MacBookPro.local>
2020-09-28 13:53:39 +0900

    20200928_1353 commit

--- SoyStudy/py/05_FlattenConvolution.py
+++ SoyStudy/py/05_FlattenConvolution.py
@@ -18,12 +18,12 @@ for i in range(2):
         feature[i*2+j] = s
 print(feature)
 
-W = len(image)**0.5
-F = len(kernel)**0.5
-P = 0
-S = 1 
+W = len(image)**0.5 # 이미지 크기 # image = [1,2,3,5,3,2,2,6,3]
+F = len(kernel)**0.5 # 커널 사이즈 # kernel = [1,0,1,1]
+P = 0 # 패딩의 유무 0이니까 패딩이 없고 1이면 패딩이 있음 패딩이 위, 아레, 좌, 우 어디로 붙는지는 아직 모름 pytorch하고 tensorflow의 패딩 붙는 방식이 다른다
+S = 1 # 스트라이드 커널 사이즈를 옮겨가는 간격
 
-print(((W-F+2*P )/S)+1)
+print(((W-F+2*P )/S)+1) # 컨볼루전 레이어 하나를 거치고 난후의 피처맵의 크기
 
 def conv(image, kernel, image_len=int(len(image)**0.5), kernel_len=int(len(kernel)**0.5), stride=1, padding=0):
 
