2354a896c700b66d42b671e4ea5c4395c9790c22
KOOKDONGHUN <dh3978@naver.com>
2020-10-09 09:23:53 +0900

    20201009_0919 commit

--- keras/keras64_fashion_cnn.py
+++ keras/keras64_fashion_cnn.py
@@ -1,7 +1,8 @@
-from keras.datasets import fashion_mnist
-from keras.utils import np_utils
-from keras.models import Sequential, Model
-from keras.layers import Dense, Conv2D, LSTM , Flatten, Dropout, MaxPooling2D , Input
+from tensorflow.keras.datasets import fashion_mnist
+# from tensorflow.keras.utils import np_utils
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Conv2D, LSTM , Flatten, Dropout, MaxPooling2D , Input
 import matplotlib.pyplot as plt
 
 (x_train, y_train),(x_test, y_test) = fashion_mnist.load_data() # 흑백이미지를 불러옴 
@@ -26,8 +27,10 @@ print(f"y_train.shape : {y_train.shape}") # y_train.shape : (60000,)
 print(f"y_test.shape : {y_test.shape}") # y_test.shape : (10000,)
 
 # 다중분류 모델에서 y값에 대한 원-핫 인코딩 
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
+# y_train = np_utils.to_categorical(y_train)
+# y_test = np_utils.to_categorical(y_test)
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
 # 0~ 255 사이의 x 값을 0~1사이의 값으로 바꿔줌 
 x_train = x_train.reshape(60000,28,28,1).astype('float32')/255
@@ -62,7 +65,7 @@ model.summary()
 # 3. 컴파일(훈련준비),실행(훈련)
 model.compile(optimizer='adam',loss = 'categorical_crossentropy', metrics = ['acc'])
 
-hist = model.fit(x_train,y_train,epochs=30,batch_size=80,callbacks=[],verbose=2)
+hist = model.fit(x_train,y_train,epochs=30,batch_size=128,callbacks=[],verbose=2)
 
 plt.plot(hist.history['loss'])
 plt.plot(hist.history['acc'])
@@ -74,7 +77,7 @@ plt.legend(['train loss','train acc'])
 plt.show()
 
 # 4. 평가, 예측
-loss,acc = model.evaluate(x_test,y_test,batch_size=80)
+loss,acc = model.evaluate(x_test,y_test,batch_size=128)
 
 print(f"loss : {loss}")
 print(f"acc : {acc}") # acc : 0.9077000021934509
